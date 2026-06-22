73ebed46f9aebb9a639d04d6cf16722dce320e7f
kpfvr <kenfavero@gmail.com>
2026-06-21 22:18:36 -0300

    cambio para home

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/mi-ministerio/',
   plugins: [
     react(),
     legacy()
